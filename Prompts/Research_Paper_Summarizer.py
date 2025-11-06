77ff9e34c2fa92711056a1419950573ebaa90362
Tanish Rajput <tanishrajput9@gmail.com>
2025-11-06 15:54:22 +0530

    Fix: Loaded the prommpt using load_prompt()

--- Prompts/Research_Paper_Summarizer.py
+++ Prompts/Research_Paper_Summarizer.py
@@ -1,7 +1,7 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, load_prompt
 
 load_dotenv()
 
@@ -17,21 +17,23 @@ length_input = st.selectbox("Select Explanation Length",["Make your selection","
 
 
 # Template
-template = PromptTemplate(
-    template = """
-    Please summarize the research paper titled "{paper_input}" with the following specifications:
-    Explanation Style: {style_input}
-    Explanation Length: {length_input}
-    1. Mathematical Details:
-        - Include relevant mathematical equations if present in the paper.
-        - Explain the mathematical concepts using simple, intutive code snippets where applicable.
-    2. Analogies:
-        - Use relatable analogies to simplify complex ideas.
-    If certain information is not available in the paper, respond with: "Insufficient information system available" instead of guessing.
-    Ensure the summary is clear, accurate, and aligned with the provided style and length.
-    """,
-    input_variables=['paper_input','style_input','length_input']
-)
+# template = PromptTemplate(
+#     template = """
+#     Please summarize the research paper titled "{paper_input}" with the following specifications:
+#     Explanation Style: {style_input}
+#     Explanation Length: {length_input}
+#     1. Mathematical Details:
+#         - Include relevant mathematical equations if present in the paper.
+#         - Explain the mathematical concepts using simple, intutive code snippets where applicable.
+#     2. Analogies:
+#         - Use relatable analogies to simplify complex ideas.
+#     If certain information is not available in the paper, respond with: "Insufficient information system available" instead of guessing.
+#     Ensure the summary is clear, accurate, and aligned with the provided style and length.
+#     """,
+#     input_variables=['paper_input','style_input','length_input']
+# )
+
+template = load_prompt('template.json')
 
 # Fill the Placeholders
 prompt = template.invoke({
